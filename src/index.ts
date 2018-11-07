7129c4267890e5df50c744ee1b4678537b0b28af
Oliver Wegner <void1976@gmail.com>
2018-11-07 18:35:47 +0100

    implementing pagination for getContacts

--- src/index.ts
+++ src/index.ts
@@ -2,51 +2,75 @@ import { Adapter, Config, Contact, PhoneNumber, start } from "@clinq/bridge";
 import { Request } from "express";
 import axios from "axios";
 
-const API_URL_CONTACTS = 'https://api2.frontapp.com/contacts';
+const API_CONTACTS_URL   = 'https://api2.frontapp.com/contacts';
+const API_CONTACTS_LIMIT = 100;
 
-const convertContact = (frontContact): Contact => ({
-    id: String(frontContact.id),
-    name: frontContact.name,
-    email: frontContact.handles
-        .filter(handle => handle.source === 'email')
-        .map(handle => handle.handle)[0]
-        || null,
-    company: null,
-    contactUrl: frontContact._links.self,
-    avatarUrl: frontContact.avatar_url,
-    phoneNumbers: frontContact.handles
-        .filter(handle => handle.source === 'phone')
+const convertContactToClinq = (frontContact): Contact => {
+    const phoneNumbers = frontContact.handles
+        .filter(handle => handle.source === 'phone' && handle.handle && handle.handle.length > 0)
         .map(handle => ({
             label: null,
             phoneNumber: handle.handle
-        }))
-});
+        }));
 
-const isValidContact = (contact: Contact): boolean =>
-    Array.isArray(contact.phoneNumbers) && contact.phoneNumbers.length > 0;
-
-const getContacts = async (accessToken: string) => {
-    const response = await axios.get(API_URL_CONTACTS, {
-        headers: {
-            Authorization: `Bearer ${accessToken}`
+    return phoneNumbers.length > 0
+        ? {
+            id: String(frontContact.id),
+            name: frontContact.name,
+            email: frontContact.handles
+                .filter(handle => handle.source === 'email')
+                .map(handle => handle.handle)[0]
+                || null,
+            company: null,
+            contactUrl: frontContact._links.self,
+            avatarUrl: frontContact.avatar_url,
+            phoneNumbers: phoneNumbers
         }
-    });
+        : null;
+};
 
-    if (!Array.isArray(response.data._results)) {
+const convertContactsToClinq = (frontContacts): Contact[] => {
+    if (!Array.isArray(frontContacts)) {
         return [];
     }
 
-    return response.data._results
-        .map(convertContact)
-        .filter(contact => isValidContact(contact));
+    return frontContacts
+        .map(convertContactToClinq)
+        .filter(contact => contact);
+};
+
+const getContacts = async (accessToken: string) => {
+    const getNextChunk = async (contacts: Contact[], url: string) =>{
+        const response = await axios.get(url, {
+            headers: {
+                Authorization: `Bearer ${accessToken}`
+            },
+            params: {
+                limit: API_CONTACTS_LIMIT
+            }
+        });
+
+        const allContacts = [
+            ...contacts,
+            ...convertContactsToClinq(response.data._results)
+        ];
+
+        const nextUrl = response.data._pagination.next;
+        if (nextUrl) {
+            return getNextChunk(allContacts, nextUrl);
+        } else {
+            return allContacts;
+        }
+
+    };
+
+    return getNextChunk([], API_CONTACTS_URL);
 };
 
 class FrontAdapter implements Adapter {
     public async getContacts(config: Config): Promise<Contact[]> {
         const { apiKey } = config;
-        const results = await getContacts(apiKey);
-        //console.log('xxxxxxxxxxxxxxxxxx', results);
-        return results;
+        return await getContacts(apiKey);
     }
 }
 
